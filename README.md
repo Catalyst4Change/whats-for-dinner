d56bab784b41a7035e131e135f782d290fa70cf1
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-07-24 13:29:51 -0600

    Attempted CYOA, Attached README

--- README.md
+++ README.md
@@ -1,23 +1,15 @@
-# What's for Dinner?
+"What's For Dinner?" solo project
+Turing 2207 Mod 1
+developed by Catalyst
 
-## Overview
+This website was designed to releive the user of the burden of chosing what to make for dinner. 
+At Iteration 0, it replicates the visual reference with a image background, header, and two dialog boxes. Having been initially intimidated by CSS & HTML, I'm proud of the way this came out. I'm aware FlexBox is doing most of the heavy lifting here, but it looks great.
 
-What's for dinner? The ultimate question. You will be building an app that helps users choose a recipe and put together meals. 
+At iteration 1, functionality is implimented. The meal type radio buttons recieve input, captured when the "Let's Cook!" button is pressed. It then chooses randomly from the respective meal type array and displays the result. This works regardless of what radio is chosen and displays one of each array when a full meal is indicated. This text replaces the cookpot vector image when implimented.
 
-This Solo Challenge gives students and instructors the opportunity to get a pulse on where you are with the foundational concepts of Module 1 curriculum. Students should use this as an opportunity to challenge themselves and work completely independently. Google can (and probably should!) be used, but any other code base should not be referenced. Instructors will be able to use your work, both completion of functionality and code quality, to determine where you stand and if you are behind for this point in the module, provide supports to intervene.
+Iteration 2 was a partial success*. It is also a technical failure. My stretch-goal was to sort each meal selection by subcategories. I started by changing the meal strings into meal objects containing boolean values of each style of cookery.
+I was hoping to capture the meal choice and pass that entire array to a function that would splice out any meal objects whose relevant keys return false, according to the style-choice radio selection. The mutated array would finally be sent to the function that chooses a random meal from that now curated array.
 
-The spec for this project can be found [here](https://frontend.turing.io/projects/module-1/dinner.html). 
-
-## Learning Goals
-
-- Gain experience building an application that utilizes HTML, CSS and JavaScript
-- Write HTML and CSS to match a provided comp
-- Understand how to listen to and respond to user events
-- Individualize your programming skill set
-
-## Setup
-
-- Fork this project to your own Github account
-- Clone the repository to your local machine
-- `cd` into the project
-- Read this README thoroughly, then begin working!
+* I did not accomplish my goal of sorting the meals by subcatagory. However, in the process of trying I taught myself a lot about exponential convolutions - meaning I made it so complicated I couldn't figure out how to do what I wanted. I can't even imagine how I would sort and display the 'whole meal' option. With more time, instructuion, and help, I believe I could eventually figure this out.
+My biggest issue in this project was trying to get the right information from the HTML radio buttons. They never seemed to return the data format that I expected. I also had to re-invoke the buttons' .value variable at every step, making the code very un-DRY. Whenever I tried to make it a global variable and reference it later, it returned undefined.
+I left my goofy, broken code for review. I would be happy with guidance to make it work. I think I'm pretty close. Currently, the meal selection section still works great, but when a subcategory option is chosen, it will no longer generate meals.
--- main.js
+++ main.js
@@ -1,73 +1,113 @@
 var questionsBox = document.querySelector('.item-1');
 var answersBox = document.querySelector('.item-2');
 var letsCookButton = document.querySelector('.lets-cook');
+
 var sidesMenu = [
-    'Miso Glazed Carrots',
-    'Coleslaw',
-    'Garden Salad',
-    'Crispy Potatoes',
-    'Sweet Potato Tots',
-    'Coconut Rice',
-    'Caeser Salad',
-    'Shrimp Summer Rolls',
-    'Garlic Butter Mushrooms',
-    'Hush Puppies'
-]
-    
+    {name:'Miso Glazed Carrots', gf:false, veg:true, vgn:true},
+    {name:'Coleslaw', gf:true, veg:true, vgn:true},
+    {name:'Garden Salad', gf:true, veg:true, vgn:true},
+    {name:'Crispy Potatoes', gf:true, veg:true, vgn:false},
+    {name:'Sweet Potato Tots', gf:false, veg:true, vgn:true},
+    {name:'Coconut Rice', gf:true, veg:true, vgn:true},
+    {name:'Caeser Salad', gf:true, veg:true, vgn:true},
+    {name:'Shrimp Summer Rolls', gf:true, veg:false, vgn:false},
+    {name:'Garlic Butter Mushrooms', gf:true, veg:true, vgn:true},
+    {name:'Hush Puppies', gf:false, veg:true, vgn:true},
+]  
 var mainsMenu = [
-    'Spaghetti and Meatballs',
-    'Pineapple Chicken',
-    'Shakshuka',
-    'Thai Yellow Curry',
-    'Bibimbap',
-    'Chicken Parmesean',
-    'Butternut Squash Soup',
-    'BBQ Chicken Burgers',
-    'Ramen',
-    'Empanadas',
-    'Chicken Fried Rice',
-    'Sheet Pan Fajitas',
-    'Margarita Pizza',
-]
-    
+    {name:'Spaghetti and Meatballs', gf:true, veg:false, vgn:false},
+    {name:'Pineapple Chicken', gf:true, veg:false, vgn:false},
+    {name:'Shakshuka', gf:true, veg:true, vgn:false},
+    {name:'Thai Yellow Curry', gf:true, veg:true, vgn:false},
+    {name:'Bibimbap', gf:true, veg:true, vgn:false},
+    {name:'Chicken Parmesean', gf:true, veg:true, vgn:true},
+    {name:'Butternut Squash Soup', gf:true, veg:true, vgn:true},
+    {name:'BBQ Chicken Burgers', gf:true, veg:false, vgn:false},
+    {name:'Ramen', gf:false, veg:true, vgn:true},
+    {name:'Empanadas', gf:false, veg:false, vgn:false},
+    {name:'Chicken Fried Rice', gf:false, veg:false, vgn:false},
+    {name:'Sheet Pan Fajitas', gf:false, veg:false, vgn:false},
+    {name:'Margarita Pizza', gf:false, veg:true, vgn:false},
+]  
 var dessertsMenu = [
-    'Apple Pie',
-    'Lemon Meringue Pie',
-    'Black Forest Cake',
-    'Banana Bread',
-    'Peach Cobbler',
-    'Cheesecake',
-    'Funfetti Cake',
-    'Baklava',
-    'Flan',
-    'Macarons',
-    'Macaroons',
-    'Chocolate Cupcakes',
-    'Pavlova',
-    'Pumpkin Pie',
-    'Key Lime Pie',
-    'Tart Tatin',
-    'Croissants',
-    'Eclairs',
+    {name:'Apple Pie', gf:false, veg:true, vgn:false},
+    {name:'Lemon Meringue Pie', gf:false, veg:true, vgn:false},
+    {name:'Black Forest Cake', gf:true, veg:true, vgn:false},
+    {name:'Banana Bread', gf:false, veg:true, vgn:false},
+    {name:'Peach Cobbler', gf:false, veg:true, vgn:false},
+    {name:'Cheesecake', gf:false, veg:true, vgn:false},
+    {name:'Funfetti Cake', gf:false, veg:true, vgn:false},
+    {name:'Baklava', gf:true, veg:true, vgn:false},
+    {name:'Flan', gf:true, veg:true, vgn:false},
+    {name:'Macarons', gf:false, veg:true, vgn:false},
+    {name:'Macaroons', gf:true, veg:true, vgn:false},
+    {name:'Chocolate Cupcakes', gf:false, veg:true, vgn:false},
+    {name:'Pavlova', gf:true, veg:true, vgn:false},
+    {name:'Pumpkin Pie', gf:false, veg:true, vgn:true},
+    {name:'Key Lime Pie', gf:false, veg:true, vgn:false},
+    {name:'Tart Tatin', gf:true, veg:true, vgn:true},
+    {name:'Croissants', gf:false, veg:true, vgn:false},
+    {name:'Eclairs', gf:false, veg:true, vgn:false},
 ]
 
-letsCookButton.addEventListener('click', letsCook);
+letsCookButton.addEventListener('click', checkConsideration);
 
-function getRandomDish(array) {
-    var random = Math.floor(Math.random() * array.length);
-    return (random, array[random]);
-  }
+function checkConsideration() {
+    var styleChoiceTF = document.querySelector('input[name="style"]:checked');
+    if (styleChoiceTF) {
+        filterMenu();
+    } else {
+        letsCook();
+    }
+}
 
-function letsCook() {
-    var foodChoice = document.querySelector('input[name="choice"]:checked').value;
+function filterMenu() {
+    var foodChoice = document.querySelector('input[name="choice"]:checked').value
     if (foodChoice === 'sides') {
-        displayMeal(getRandomDish(sidesMenu));
+        parseMenu(sidesMenu);
     }
     if (foodChoice === 'mains') {
-        displayMeal(getRandomDish(mainsMenu));
+        parseMenu(mainsMenu);
     }
     if (foodChoice === 'desserts') {
-        displayMeal(getRandomDish(dessertsMenu));
+        parseMenu(dessertsMenu);
+    }
+    if (foodChoice === 'entire-meal') {
+        parseMenu(oneOfEach);
+    }
+}
+
+function parseMenu(menuList) {
+    //remove all index items that do NOT return gf=True
+    var styleChoice = document.querySelector('input[name="style"]:checked').value;
+    console.log(styleChoice);
+    var filteredMenu = [];
+    for (let i = 0; i < menuList.length; i++) {
+        if (menuList[i][styleChoice] === true) {
+            filteredMenu.push(menuList[i]);
+        }
+    }
+    console.log(filteredMenu);
+
+    // forEach((menuName) => {
+    //     if (menuName.gf != true) {
+    //         menuName.splice([element], 1)
+    //     }
+    // });
+    //then pass that new array to letsCook()
+    // shit will that even work or do i have to bake it into this functuion?
+}
+
+function letsCook() {
+    var foodChoice = document.querySelector('input[name="choice"]:checked').value
+    if (foodChoice === 'sides') {
+        displayMeal(getRandomDish(sidesMenu).name);
+    }
+    if (foodChoice === 'mains') {
+        displayMeal(getRandomDish(mainsMenu).name);
+    }
+    if (foodChoice === 'desserts') {
+        displayMeal(getRandomDish(dessertsMenu).name);
     }
     if (foodChoice === 'entire-meal') {
         oneOfEach();
@@ -82,17 +122,26 @@ function displayMeal(food) {
 
 function oneOfEach() {
     answersBox.innerHTML = `
-    <p>Start with<br>${getRandomDish(sidesMenu)},<br>
-    then have<br>${getRandomDish(mainsMenu)},<br>
-    and for dessert have<br>${getRandomDish(dessertsMenu)}!
+    <p>Start with<br>${getRandomDish(sidesMenu).name},<br>
+    then have<br>${getRandomDish(mainsMenu).name},<br>
+    and for dessert have<br>${getRandomDish(dessertsMenu).name}!
     </p>
     `;
 }
 
-answersBox.innerHTML = `
-    <img
-    src="assets/cookpot.svg"
-    alt="cookpot"
-    height="150"
-    width="150" />
-`;
+function getRandomDish(array) {
+    var random = Math.floor(Math.random() * array.length);
+    return (random, array[random]);
+}
+
+// unsused functions
+
+var radioNodeList = document.querySelectorAll('input[name="choice"]:checked');;
+function radioTest() {
+    for (let i = 0; i < radioNodeList.length; i++) {
+        var isChecked = (radioNodeList[i].checked)
+        if (isChecked[i]) {
+            console.log(isChecked[i]);
+        }
+    }    
+}
